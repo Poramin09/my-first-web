e7670b6541e5ae5f7372e41f52c52ef18e0f05ae
Poramin09 <poraminvk2929@gmail.com>
2026-01-16 18:10:49 +0700

    Update web.py

--- web.py
+++ web.py
@@ -57,7 +57,7 @@ st.markdown(f"""
 
 #Logic(home)
 if st.session_state.page == 'home':
-    st.markdown('<div class="main-banner"><h2.5>พรรค สดุมาส่งแล้วคร้าฟ📦✨</h2.5></div>', unsafe_allow_html=True)
+    st.markdown('<div class="main-banner"><h3>พรรค สดุมาส่งแล้วคร้าฟ📦✨</h3></div>', unsafe_allow_html=True)
     st.write("##")
     st.write("##")
     col1, col2 = st.columns(2)
@@ -111,6 +111,7 @@ elif st.session_state.page == 'member':
         st.session_state.page = 'home'
 
         st.rerun()
+
 
 
 
